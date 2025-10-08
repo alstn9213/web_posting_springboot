3a071795984df292e48334bac83c4cc440427d41
alstn9213 <alstn9213@naver.com>
2025-10-08 13:14:38 +0900

    first commit

---
+++ src/main/java/com/example/demo/cotroller/MemberController.java
@@ -0,0 +1,64 @@
+package com.example.demo.cotroller;
+
+import com.example.demo.dto.ArticleRequest;
+import com.example.demo.dto.ArticleResponse;
+import com.example.demo.dto.MemberRequest;
+import com.example.demo.dto.MemberResponse;
+import com.example.demo.model.Member;
+import com.example.demo.model.MemberRepository;
+import com.example.demo.service.ArticleService;
+import com.example.demo.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+//    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+    private final ArticleService articleService;
+
+//    @PostMapping
+//    public Member post(@RequestBody Member member) {
+//        return memberRepository.save(member);
+//    }
+//
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberResponse post(@RequestBody MemberRequest memberRequest) {
+        return memberService.create(memberRequest);
+    }
+
+    @GetMapping
+    public List<MemberResponse> getAll() {
+        return memberService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public MemberResponse get(@PathVariable("id") Long id) {
+        return memberService.findById(id);
+    }
+    @PutMapping("/{id}")
+    public MemberResponse put(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.update(id, memberRequest);
+    }
+
+    @PatchMapping("/{id}")
+    public MemberResponse patch(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.patch(id, memberRequest);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        memberService.deleteById(id);
+    }
+
+    @PostMapping("/{id}/articles")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArticleResponse postArticle(@PathVariable("id") Long id, @RequestBody ArticleRequest articleRequest) {
+        return articleService.create(id, articleRequest);
+    }
+}
